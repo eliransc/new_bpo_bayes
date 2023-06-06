aeea47c354914c240202963274963e5b94c8f0ae
eliransc <scherzter@gmail.com>
2023-06-06 16:07:22 +0300

    first commit

--- bayesopt.py
+++ bayesopt.py
@@ -21,8 +21,8 @@ from tqdm import tqdm
 import numpy as np
 
 import matplotlib.pyplot as plt
-from skopt.plots import plot_gaussian_process
-from skopt import gp_minimize
+# from skopt.plots import plot_gaussian_process
+# from skopt import gp_minimize
 from bayes_opt.logger import JSONLogger
 from bayes_opt.event import Events
 from bayes_opt import BayesianOptimization
