9f6d08ab7be1eb41c22b56e4ae250b395e68589d
eliransc <scherzter@gmail.com>
2023-06-06 14:05:56 +0300

    first commit

--- sbatch_narval_sample.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#SBATCH -t 0-20:58
-#SBATCH -A def-dkrass
-#SBATCH --mem 10000
-source /home/eliransc/projects/def-dkrass/eliransc/queues/bin/activate
-python /home/eliransc/projects/def-dkrass/eliransc/bpo_toy_problem/train_Eliran.py
