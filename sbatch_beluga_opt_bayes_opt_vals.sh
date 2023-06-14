d7b5e78b77527700b62063f857f2186347244ade
eliransc <scherzter@gmail.com>
2023-06-14 16:36:15 +0300

    first commit

--- sbatch_beluga_opt_bayes_opt_vals.sh
+++ sbatch_beluga_opt_bayes_opt_vals.sh
@@ -3,4 +3,4 @@
 #SBATCH -A def-dkrass
 #SBATCH --mem-per-cpu=10000M
 source /home/eliransc/projects/def-dkrass/eliransc/queues/bin/activate
-python /home/eliransc/projects/def-dkrass/eliransc/new_bpo_bayes/bayesopt.py
+python /home/eliransc/projects/def-dkrass/eliransc/new_bpo_bayes/train_SVFA.py
