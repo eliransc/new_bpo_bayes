329ffa6df2f07f7225b0de670116ca2ec88408c5
eliransc <scherzter@gmail.com>
2023-06-14 16:14:21 +0300

    first commit

--- train_SVFA.py
+++ train_SVFA.py
@@ -13,7 +13,7 @@ import pickle as pkl
 # -Get the total_reward
 def simulate_competition(A):
 
-    simulator_fake = Simulator(running_time, ShortestProcessingTime(), config_type='complete_reversed', reward_function='AUC')
+    simulator_fake = Simulator(running_time, ShortestProcessingTime(), config_type='high_utilization', reward_function='AUC')
     a1 = A[0]
     a2 = A[1]
     a3 = A[2]
@@ -26,7 +26,7 @@ def simulate_competition(A):
     # planner1 = ShortestProcessingTime()
 
     # The config types dictates the system
-    simulator = Simulator(running_time, planner, config_type='complete_reversed', reward_function='AUC')
+    simulator = Simulator(running_time, planner, config_type='high_utilization', reward_function='AUC')
     # You can access some proporties from the simulation:
     # simulator.resource_pools: for each tasks 1) the resources that can process it and 2) the mean and variance of the processing time of that assignment
     # simulator.mean_interarrival_time
@@ -53,31 +53,41 @@ def aggregate_sims(a1, a2, a3, a4, a5, a6, a7):
     model_num = np.random.randint(0, 1000)
     tot_res = []
 
-    for ind in range(3):
+    for ind in range(10):
         res = simulate_competition(A)
         # print(res)
         tot_res.append(res)
 
-    pkl.dump((a1, a2, a3, a4, a5, a6, a7, tot_res), open('single_bayes_' + 'complete_reversed' + str(model_num) + '.pkl', 'wb'))
+        pkl.dump((a1, a2, a3, a4, a5, a6, a7, tot_res), open('high_utilization_'+ str(ind) + '_' + str(model_num) + '.pkl', 'wb'))
 
     return -np.array(tot_res).mean()
 
+ # open('single_bayes_' + 'high_utilization' + str(model_num) + '.pkl', 'wb'))
 
 def main():
 
-    # high_utilization = [1.321091, 4.556206,  0.790609,0.000000, 3.051695,    15.739693]
+    high_utilization = [0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 4.620278449742129]
 
-    #low_utilization = [1.209298, 4.898732,	0.496618, 2.658753,	2.234656,12.339795]
+    low_utilization = [0.15, 0.15, 20.0, 0.15, 0.15, 0.15, 20.0]
 
-    # n_system = [0.000000,	5.000000,	1.584558,	0.000000,	0.000000	, 7.0]
-    #slow = [5.000000,	3.767883,	0.0,	1.645102,	0.000000,	20.000000]
+    n_system = [0.015, 0.015, 20.0, 20.0, 6.173545342831394, 0.015, 10.0]
+    slow = [5.000000,	3.767883,	0.0,	1.645102,	0.000000,	20.000000]
 
-    #down_stream = [1.655077, 4.013198,0.491260, 0.279967, 2.213313, 7.287871]
+    down_stream = [0.15, 0.15, 20.0, 20.0, 18.911578855212127, 0.15, 20.0]
 
-    # complete_all = [0.337376	4.110818	1.167821	0.862430	0.474204	19.111311]
+    complete = [3.5891129717483348, 0.015, 19.558505455930856, 7.3067908967685895, 2.4903986035491594, 10.125089778688992, 34.53567091112935]
 
+    slow_server = [0.15, 7.96025975421663, 10.22775090867394, 20.0, 4.257145518125576, 0.15, 20.0]
 
-    # simulate_competition(A)
+    parallel = [0.15, 0.15, 20.0, 20.0, 20.0, 0.15, 8.28753107297809]
+
+    complete_reversed = [0.5704487247166272, 8.862502952056776, 6.585704320595577, 17.936281295072106, 1.9628574164600667, 18.412941809480696, 29.918175881953324]
+
+    complete_parallel = [2.384444525797204, 0.15,0.15,20.0,1.6446776889775383,6.152011577643852,2.081398819447721]
+
+   # simulate_competition(A)
+
+    A = high_utilization
 
     get_results = aggregate_sims(A)
 
