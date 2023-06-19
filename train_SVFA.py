2323d10519d65628e8cf0ed40f3d51a7bfab7aaa
eliransc <scherzter@gmail.com>
2023-06-19 08:30:51 +0300

    first commit

--- train_SVFA.py
+++ train_SVFA.py
@@ -13,7 +13,7 @@ import pickle as pkl
 # -Get the total_reward
 def simulate_competition(A):
 
-    simulator_fake = Simulator(running_time, ShortestProcessingTime(), config_type='complete_reversed', reward_function='AUC')
+    simulator_fake = Simulator(running_time, ShortestProcessingTime(), config_type='complete', reward_function='AUC')
     a1 = A[0]
     a2 = A[1]
     a3 = A[2]
@@ -26,7 +26,7 @@ def simulate_competition(A):
     # planner1 = ShortestProcessingTime()
 
     # The config types dictates the system
-    simulator = Simulator(running_time, planner, config_type='complete_reversed', reward_function='AUC')
+    simulator = Simulator(running_time, planner, config_type='complete', reward_function='AUC')
     # You can access some proporties from the simulation:
     # simulator.resource_pools: for each tasks 1) the resources that can process it and 2) the mean and variance of the processing time of that assignment
     # simulator.mean_interarrival_time
@@ -53,12 +53,12 @@ def aggregate_sims(A): # a1, a2, a3, a4, a5, a6, a7
     model_num = np.random.randint(0, 1000)
     tot_res = []
 
-    for ind in range(10):
+    for ind in range(1):
         res = simulate_competition(A)
         # print(res)
         tot_res.append(res)
 
-    pkl.dump((A, tot_res), open('complete_reversed_optimal_' + str(model_num) + '.pkl', 'wb'))
+    pkl.dump((A, tot_res), open('complete_opt1_' + str(model_num) + '.pkl', 'wb'))
 
     return -np.array(tot_res).mean()
 
